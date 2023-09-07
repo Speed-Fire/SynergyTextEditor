455b73a0731b2b73689330898101c9edd35a8199
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-09-07 16:54:05 +0200

    Adding simple Blocker.

---
+++ SynergyTextEditor/Classes/Blockers/Blocker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynergyTextEditor.Classes.Blockers
+{
+    public class Blocker : IBlocker
+    {
+        private volatile bool _isBlocked = false;
+        private readonly object _locker = new();
+
+        public bool IsBlocked()
+        {
+            lock (_locker)
+            {
+                return _isBlocked;
+            }
+        }
+
+        public void SetState(bool isBlocked)
+        {
+            lock(_locker)
+            {
+                _isBlocked = isBlocked;
+            }
+        }
+    }
+}
